9452b821ed2cd97f51cc9beeb6570fc987ff51d6
lopez-mgu <lopez.mgu@gmail.com>
2022-08-06 18:54:04 -0600

    heroku modifications v1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./src/app');
 require('dotenv').config();
 require ('./src/db/mongodb');
 
-const PORT = process.env.PORTENV || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`)
